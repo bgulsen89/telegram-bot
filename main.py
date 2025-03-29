6a2aa8eb15e7f6d06848d6d5b29e09c44da18400
bgulsen89 <bgulsen89@icloud.com>
2025-03-29 16:13:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater
 from config import TOKEN
+from modules import onay_sistemi
 from modules.karsilama import karsilama_ekle
 from modules.reklam_filtresi import reklam_filtresi_ekle
 from modules.test_komutlari import test_komutlarini_ekle
@@ -12,6 +13,7 @@ def main():
     karsilama_ekle(dp)
     reklam_filtresi_ekle(dp)
     test_komutlarini_ekle(dp)
+    onay_sistemi.kayit_ol(dp)
 
     updater.start_polling()
     updater.idle()
