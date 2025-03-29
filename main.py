7a71d3f99a7554695eb8f614766957f571d046d0
bgulsen89 <bgulsen89@icloud.com>
2025-03-29 16:14:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from telegram.ext import Updater
 from config import TOKEN
-from modules import onay_sistemi
-from modules.karsilama import karsilama_ekle
-from modules.reklam_filtresi import reklam_filtresi_ekle
-from modules.test_komutlari import test_komutlarini_ekle
+from modules import onay_butonu
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Modülleri ekle
-    karsilama_ekle(dp)
-    reklam_filtresi_ekle(dp)
-    test_komutlarini_ekle(dp)
-    onay_sistemi.kayit_ol(dp)
+    # Onay sistemini ekle
+    onay_butonu.kayit_ol(dp)
 
     updater.start_polling()
     updater.idle()
